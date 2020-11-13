9db0d1e86efdacfdfae4bddbcd64819b7a229e53
Azat Vakhitov <32743980+qtakmalay@users.noreply.github.com>
2020-11-14 00:31:39 +0500

    connected DB and starting to write a proper DataGrid

---
+++ Interes/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Data.Entity.Core.Objects;
+
+namespace Interes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MagazinEntityModel dataEntities = new MagazinEntityModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query =
+            select item.name 
+            from item in dataEntities.items;
+
+            dataGrid1.ItemsSource = query.ToList();
+        }
+
+    }
+}
